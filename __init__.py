d3d2bad6abba5d575cf322b5c400560eca5454ba
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-24 23:00:09 +0200

    Info

---
+++ __init__.py
@@ -0,0 +1,13 @@
+__dependencies__ = {
+    'necessary':
+        {
+            'pygame': '2.0.1',
+            'PyOpenGL': '3.1.5',
+            'PyOpenGL-accelerate': '3.1.5',
+        },
+}
+__doc__ = \
+    """
+    OpenGL tutorial with PyOpenGL and Pygame according to Lee Stemkoski, Michael Pascale, 
+    'Developing Graphics Frameworks with Python and OpenGL', CRC Press, 2022
+    """
