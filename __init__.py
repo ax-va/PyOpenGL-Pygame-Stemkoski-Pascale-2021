37ad65633135b2b200c358c36604b11ae239b974
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-08-25 20:15:46 +0200

    Info

--- __init__.py
+++ __init__.py
@@ -7,8 +7,7 @@ __dependencies__ = {
             'numpy': '1.21.1',
         },
 }
-__doc__ = \
-    """
+__doc__ = """
     OpenGL tutorial with PyOpenGL and Pygame based on Lee Stemkoski's and Michael Pascale's 
     book "Developing Graphics Frameworks with Python and OpenGL" published by CRC Press, 2022
     """
