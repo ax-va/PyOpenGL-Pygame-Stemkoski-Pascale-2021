cde12fd14245d503eb657f304b547d9f9e59d09d
Alexander Vasiliev <vasiliev.informatik@t-online.de>
2021-11-09 14:31:46 +0100

    Restructuring

--- tutorial/tests/test-2-07.py
+++ tutorial/tests/test-2-07.py
@@ -1,4 +1,12 @@
 import OpenGL.GL as GL
+import pathlib
+import sys
+
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[2])
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
 
 from tutorial.core.base import Base
 from tutorial.core.utils import Utils
