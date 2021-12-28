979281632b757fd41698d3b5e1e9e31339a5a029
Alex Vasiliev <vasiliev.informatik@t-online.de>
2021-12-28 23:02:11 +0100

    Object3D added

---
+++ pyopengl_pygame/scene_ext/object_3d.py
@@ -0,0 +1,96 @@
+from pyopengl_pygame.core.matrix import Matrix
+
+
+class Object3D:
+    """  The Object3D class represents a node in the scene graph tree structure """
+    def __init__(self):
+        # Local transform matrix with respect to the parent of the object
+        self._transform = Matrix.make_identity()
+        self._parent = None
+        self._children = []
+
+    def add(self, child):
+        self._children.append(child)
+        child.parent = self
+
+    def remove(self, child):
+        self._children.remove(child)
+        child.parent = None
+
+    def get_world_matrix(self):
+        """  Calculate a transformation of this Object3D relative
+             to the root Object3D of the scene graph  """
+        if self._parent is None:
+            return self._transform
+        else:
+            return self._parent.get_world_matrix() @ self._transform
+
+    def get_descendant_list(self):
+        """  Return a single list containing all descendants  """
+        # master list of all descendant nodes
+        descendants = []
+        # nodes to be added to descendant list,
+        # and whose children will be added to this list
+        nodes_to_process = [self]
+        # continue processing nodes while any are left
+        while len(nodes_to_process) > 0:
+            # remove first node from list
+            node = nodes_to_process.pop(0)
+            # add this node to descendant list
+            descendants.append(node)
+            # children of this node must also be processed
+            nodes_to_process = node.children + nodes_to_process
+        return descendants
+
+    def apply_matrix(self, matrix, local=True):
+        if local:
+            self._transform = self._transform @ matrix
+        else:
+            self._transform = matrix @ self._transform
+
+    def translate(self, x, y, z, local=True):
+        m = Matrix.make_translation(x, y, z)
+        self.apply_matrix(m, local)
+
+    def rotate_x(self, angle, local=True):
+        m = Matrix.make_rotation_x(angle)
+        self.apply_matrix(m, local)
+
+    def rotate_y(self, angle, local=True):
+        m = Matrix.make_rotation_y(angle)
+        self.apply_matrix(m, local)
+
+    def rotate_z(self, angle, local=True):
+        m = Matrix.make_rotation_z(angle)
+        self.apply_matrix(m, local)
+
+    def scale(self, s, local=True):
+        m = Matrix.make_scale(s)
+        self.apply_matrix(m, local)
+
+    def get_position(self):
+        """   Return the local position of the object (with respect to its parent)  """
+        # The position of an object can be determined from entries in the
+        # last column of the transform matrix
+        return [self._transform.item((0, 3)),
+                self._transform.item((1, 3)),
+                self._transform.item((2, 3))]
+
+    def get_world_position(self):
+        """   Return the global or world position of the object  """
+        world_transform = self.get_world_matrix()
+        return [world_transform.item((0, 3)),
+                world_transform.item((1, 3)),
+                world_transform.item((2, 3))]
+
+    def set_position(self, position):
+        """   Set the local position of the object  """
+        self._transform.itemset((0, 3), position[0])
+        self._transform.itemset((1, 3), position[1])
+        self._transform.itemset((2, 3), position[2])
+
+
+if __name__ == "__main__":
+    l1 = [1, 2, 3]
+    l2 = [4, 5, 6]
+    print(l1 + l2)
