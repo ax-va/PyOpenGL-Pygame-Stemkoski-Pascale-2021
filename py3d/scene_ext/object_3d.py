1b390e4514e180727d2984dc695f36e457c403c4
Alex Vasiliev <vasiliev.informatik@t-online.de>
2021-12-29 00:23:57 +0100

    Chapter 4 updated

--- py3d/scene_ext/object_3d.py
+++ py3d/scene_ext/object_3d.py
@@ -2,16 +2,16 @@ from py3d.core.matrix import Matrix
 
 
 class Object3D:
-    """  Represent a node in the scene graph tree structure  """
+    """ Represent a node in the scene graph tree structure """
     def __init__(self):
         # Local transform matrix with respect to the parent of the object
-        self._transform = Matrix.make_identity()
+        self._matrix = Matrix.make_identity()
         self._parent = None
         self._children = []
 
     @property
     def descendant_list(self):
-        """  Return a single list containing all descendants  """
+        """ Return a single list containing all descendants """
         # master list of all descendant nodes
         descendants = []
         # nodes to be added to descendant list,
@@ -29,16 +29,32 @@ class Object3D:
 
     @property
     def global_matrix(self):
-        """  Calculate a transformation of this Object3D relative
-             to the root Object3D of the scene graph  """
+        """ Calculate a transformation of this Object3D relative to the root Object3D of the scene graph """
         if self._parent is None:
-            return self._transform
+            return self._matrix
         else:
-            return self._parent.global_matrix @ self._transform
+            return self._parent.global_matrix @ self._matrix
+
+    @property
+    def global_position(self):
+        """ Return the global or world position of the object """
+        global_matrix = self.global_matrix
+        return [global_matrix.item((0, 3)),
+                global_matrix.item((1, 3)),
+                global_matrix.item((2, 3))]
 
     @property
     def local_matrix(self):
-        return self._transform
+        return self._matrix
+
+    @property
+    def local_position(self):
+        """ Return the local position of the object (with respect to its parent) """
+        # The position of an object can be determined from entries in the
+        # last column of the transform matrix
+        return [self._matrix.item((0, 3)),
+                self._matrix.item((1, 3)),
+                self._matrix.item((2, 3))]
 
     def add(self, child):
         self._children.append(child)
@@ -51,9 +67,11 @@ class Object3D:
     # apply geometric transformations
     def apply_matrix(self, matrix, local=True):
         if local:
-            self._transform = self._transform @ matrix
+            # local transform
+            self._matrix = self._matrix @ matrix
         else:
-            self._transform = matrix @ self._transform
+            # global transform
+            self._matrix = matrix @ self._matrix
 
     def translate(self, x, y, z, local=True):
         m = Matrix.make_translation(x, y, z)
@@ -75,23 +93,8 @@ class Object3D:
         m = Matrix.make_scale(s)
         self.apply_matrix(m, local)
 
-    def get_position(self):
-        """   Return the local position of the object (with respect to its parent)  """
-        # The position of an object can be determined from entries in the
-        # last column of the transform matrix
-        return [self._transform.item((0, 3)),
-                self._transform.item((1, 3)),
-                self._transform.item((2, 3))]
-
-    def get_world_position(self):
-        """   Return the global or world position of the object  """
-        world_transform = self.get_world_matrix()
-        return [world_transform.item((0, 3)),
-                world_transform.item((1, 3)),
-                world_transform.item((2, 3))]
-
     def set_position(self, position):
-        """   Set the local position of the object  """
-        self._transform.itemset((0, 3), position[0])
-        self._transform.itemset((1, 3), position[1])
-        self._transform.itemset((2, 3), position[2])
+        """ Set the local position of the object """
+        self._matrix.itemset((0, 3), position[0])
+        self._matrix.itemset((1, 3), position[1])
+        self._matrix.itemset((2, 3), position[2])
