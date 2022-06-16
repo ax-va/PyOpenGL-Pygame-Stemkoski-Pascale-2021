c245cce697c1595c162cd06dfdeea9c84c7f2c55
Alex Vasiliev <vasiliev.informatik@t-online.de>
2022-06-16 13:33:02 +0200

    update

--- py3d/geometry/cylindrical.py
+++ py3d/geometry/cylindrical.py
@@ -1,4 +1,4 @@
-from math import sin, cos, pi
+import math
 
 from py3d.core.matrix import Matrix
 from py3d.geometry.parametric import ParametricGeometry
@@ -10,22 +10,22 @@ class CylindricalGeometry(ParametricGeometry):
                  radial_segments=32, height_segments=4,
                  closed_top=True, closed_bottom=True):
         def surface_function(u, v):
-            return [(v * radius_top + (1 - v) * radius_bottom) * sin(u),
-                    height * (v - 0.5),
-                    (v * radius_top + (1 - v) * radius_bottom) * cos(u)]
-        super().__init__(0, 2*pi, radial_segments, 0, 1, height_segments, surface_function)
+            return [(v*radius_top + (1 - v)*radius_bottom)*math.sin(u),
+                    height*(v - 0.5),
+                    (v*radius_top + (1 - v)*radius_bottom)*math.cos(u)]
+        super().__init__(0, 2*math.pi, radial_segments, 0, 1, height_segments, surface_function)
 
         if closed_top:
             top_geometry = PolygonGeometry(radial_segments, radius_top)
             transform = Matrix.make_translation(0, height/2, 0) \
-                      @ Matrix.make_rotation_y(-pi/2) \
-                      @ Matrix.make_rotation_x(-pi/2)
+                      @ Matrix.make_rotation_y(-math.pi/2) \
+                      @ Matrix.make_rotation_x(-math.pi/2)
             top_geometry.apply_matrix(transform)
             self.merge(top_geometry)
         if closed_bottom:
             bottom_geometry = PolygonGeometry(radial_segments, radius_bottom)
             transform = Matrix.make_translation(0, -height/2, 0) \
-                      @ Matrix.make_rotation_y(-pi/2) \
-                      @ Matrix.make_rotation_x(-pi/2)
+                      @ Matrix.make_rotation_y(-math.pi/2) \
+                      @ Matrix.make_rotation_x(-math.pi/2)
             bottom_geometry.apply_matrix(transform)
             self.merge(bottom_geometry)
